428cd8ed60d025e78f9cbbf41d4b012335dbfddd
Vinay Kumar <vvinnu261@gmail.com>
2026-06-28 22:34:13 +0530

    modified

--- 3.py
+++ 3.py
@@ -1,3 +1,20 @@
-a= 1 
-b= 2
-print(a+b)
+# Operators 
+# 1. Assignment operators
+
+a = 5  # = is assign operator
+a += 10  #  a = a + 10  = 15 (This is shortform ) Output is 15
+a -= 10  # Output is 5
+a *= 10  # Output is 50 
+a /= 10  # Output is 5.0
+
+print(a)
+
+#2. Comparison operator
+
+x = 10 
+y = 100 
+print(x == y)  # == is a comparison operator Output is false
+print(x != y)  # Output is True
+print(x > y)   #Output is false
+print(x < y)  
+ 
