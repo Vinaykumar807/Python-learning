080775a39d7c4ebc24160811aeca085218bb6b1f
Vinay Kumar <vvinnu261@gmail.com>
2026-06-28 22:38:30 +0530

    modified

--- 2.py
+++ 2.py
@@ -1,20 +1,20 @@
-# # Input and output in python
+# Input and output in python
 
-# age = input("enter your age : ")
-# print("Your age is :", age)
+age = input("enter your age : ")
+print("Your age is :", age)
 
-# #Problem 1
-# boy_name= input("Enter the boy name: ")
-# girl_name= input("Enter the girl name: ")
+#Problem 1
+boy_name= input("Enter the boy name: ")
+girl_name= input("Enter the girl name: ")
 
-# print(boy_name ,"Loves", girl_name)
+print(boy_name ,"Loves", girl_name)
 
-# #Problem 2 calculate age diffrence 
+#Problem 2 calculate age diffrence 
 
-# boy_age =int(input("enter the boy age : "))
-# girl_age = int(input("Enter the girl age :"))
-# age_diff= boy_age - girl_age
-# print(f"Age Diffrence is : {age_diff} years ")
+boy_age =int(input("enter the boy age : "))
+girl_age = int(input("Enter the girl age :"))
+age_diff= boy_age - girl_age
+print(f"Age Diffrence is : {age_diff} years ")
 
 #Comments 
 
@@ -24,3 +24,4 @@ College = "Garden city univesity"  # College of the student
 CGPA = 9.10  # CGPA of the student 
  
 print(Name, "\n", Age, "\n", College, "\n", CGPA)
+# output is printed on the treminal 
