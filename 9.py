42618d110011a554b9ea3add1c83b29d7f63c0fe
Vinay Kumar <vvinnu261@gmail.com>
2026-07-07 20:17:49 +0530

    modified

--- 9.py
+++ 9.py
@@ -32,9 +32,22 @@ print(d)
 city_pop = {
     "Raichur" : 100,         # Dictionary in Dictionary
     "Devsuguru" : 80,
-    "Gandhal"  : 50,
+    "Gandhal"  : 30,
     "Jalahalli" :20,
 }
-large_pop = {x:y for x,y in city_pop.items() if y>70}   #.items() used when we pass the 2 values like (key and value) then it will craete a list and tuple 
+large_pop = {x:y for x,y in city_pop.items() if y>40}   #.items() used when we pass the 2 values like (key and value) then it will craete a list and tuple 
 print(large_pop)
 
+# String split method 
+
+s = "Hi my self Vinay Kumar!"
+l = s.split()           # .split() is used to convert the strings into lists 
+print(l)        
+
+
+x = input("Enter list of integers :").split()
+l = [int(num) for num in x]         # Taking list as a input (This a 1 method )
+print(l)
+
+x = [int(num) for num in input("Enter a list of integers : ").split()]
+print(x)                            # This is a 2 method to taking list integers as a input 
