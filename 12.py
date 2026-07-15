91c34463783aefb25b8bc50c23fec418a0ce44e2
Vinay Kumar <vvinnu261@gmail.com>
2026-07-15 19:15:11 +0530

    modified

--- 12.py
+++ 12.py
@@ -41,3 +41,13 @@ def prt_list(list, idx=0):
     prt_list(list,idx+1)
 num = [1,2,3,4,5,6]
 prt_list(num)
+
+#Comments 
+
+Name = "Vinay Kumar"  #Name of the student 
+Age = 20              # Age of the Student 
+College = "Garden city univesity"  # College of the student 
+CGPA = 9.10  # CGPA of the student 
+ 
+print(Name, "\n", Age, "\n", College, "\n", CGPA)
+# output is printed on the treminal 
